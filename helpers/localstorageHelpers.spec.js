c634ab9efa84522ff4c191b4b57c114e07f9a24b
Jimbo <marquardtjames@gmail.com>
2021-01-13 17:20:34 -0600

    add new dirs to test coverage collection. continue to adjust test specs to new paradigm

--- helpers/localstorageHelpers.spec.js
+++ helpers/localstorageHelpers.spec.js
@@ -20,43 +20,45 @@ describe('localstorageHelpers', () => {
   });
 
   // probably move this to a browserHelpers spec file.
-  it('can tell when localStorage is not available', () => {
-    isBrowser.mockImplementation(() => false);
-    expect(isBrowser()).toBe(false);
-  });
+  // it('can tell when localStorage is not available', () => {
+  //   isBrowser.mockImplementation(() => false);
+  //   expect(isBrowser()).toBe(false);
+  // });
 
-  it('can serialize notes from state object', () => {
-    const notes = ['hello', 'there'];
-    const state = { notes };
-    const expectedResult = '["hello","there"]';
-    expect(helpers.serializeNotes(state)).toEqual(expectedResult);
-  });
+  // it('can serialize notes from state object', () => {
+  //   const notes = ['hello', 'there'];
+  //   const state = { notes };
+  //   const expectedResult = '["hello","there"]';
+  //   expect(helpers.serializeNotes(state)).toEqual(expectedResult);
+  // });
 
   // it('can deserialize notes from string to array', () => {});
-  it('can load and deserialize notes from localstorage', () => {
-    isBrowser.mockImplementation(() => true);
-    localStorage.getItem.mockImplementation(() => `["yes", "no"]`);
-    const result = helpers.loadNotesFromLocalStorage();
-    expect(localStorage.getItem).toHaveBeenCalled();
-    expect(isBrowser).toHaveBeenCalled();
-    expect(result).toEqual(['yes', 'no']);
-  });
+  // it('can load and deserialize notes from localstorage', () => {
+  //   isBrowser.mockImplementation(() => true);
+  //   localStorage.getItem.mockImplementation(() => `["yes", "no"]`);
+  //   const result = helpers.loadNotesFromLocalStorage();
+  //   expect(localStorage.getItem).toHaveBeenCalled();
+  //   expect(isBrowser).toHaveBeenCalled();
+  //   expect(result).toEqual(['yes', 'no']);
+  // });
 
   // do we even want this behavior?
-  it('returns empty array if localstorage unavailable', () => {
-    isBrowser.mockReset();
-    expect(helpers.loadNotesFromLocalStorage()).toEqual([]);
+  it('returns undefined if localstorage unavailable', () => {
+    // isBrowser.mockReset();
+    isBrowser.mockImplementation(() => false);
+
+    expect(helpers.loadEntitiesFromLocalStorage()).toEqual(undefined);
   });
 
-  it('can save notes from state to localstorage', () => {
-    const state = {
-      notes: ['hello', 'hi'],
-    };
-    const expectedStoredValue = `["hello","hi"]`;
-    helpers.saveNotesToLocalStorage(state);
-    expect(localStorage.setItem).toHaveBeenCalledWith(
-      helpers.LOCALSTORAGE_KEY,
-      expectedStoredValue
-    );
-  });
+  // it('can save notes from state to localstorage', () => {
+  //   const state = {
+  //     notes: ['hello', 'hi'],
+  //   };
+  //   const expectedStoredValue = `["hello","hi"]`;
+  //   helpers.saveNotesToLocalStorage(state);
+  //   expect(localStorage.setItem).toHaveBeenCalledWith(
+  //     helpers.LOCALSTORAGE_KEY,
+  //     expectedStoredValue
+  //   );
+  // });
 });
--- jest.config.js
+++ jest.config.js
@@ -17,5 +17,7 @@ module.exports = {
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue',
     '<rootDir>/store/**/*',
+    '<rootDir>/helpers/**/*',
+    '<rootDir>/models/**/*',
   ],
 };
