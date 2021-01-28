f744ad29632a4052e099c0380b622f1dd02068ca
Jimbo <marquardtjames@gmail.com>
2021-01-27 23:49:49 -0600

    removed 'wait' from cypress test

--- cypress/integration/hello.spec.js
+++ cypress/integration/hello.spec.js
@@ -7,10 +7,8 @@ describe('hello cypress', () => {
   });
   it('can visit vega chart page', () => {
     cy.get('[href="/debug"]').click();
-    cy.wait(1000);
     cy.get('[data-cy="add-data"]').click();
     cy.get('[href="/vega-try"]').click();
-    cy.wait(1000);
     cy.get('svg').should('exist');
   });
 });
