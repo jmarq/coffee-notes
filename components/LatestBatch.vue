04741d0f6c0f1ccd3977504fa13587061f25934e
Jimbo <marquardtjames@gmail.com>
2021-02-12 16:35:07 -0600

    table for previous batch info instead of paragraphs

--- components/LatestBatch.vue
+++ components/LatestBatch.vue
@@ -2,11 +2,35 @@
   <section class="recent-batch">
     <div v-if="mostRecent" class="recent-batch-info">
       <h2 class="font-bold text-xl">Previous batch</h2>
-      <p>Bean: {{ mostRecent.bean.name }}</p>
-      <p>Grind Size: {{ mostRecent.grind_size }}</p>
-      <p>Batch Size: {{ mostRecent.batch_size_oz }} oz.</p>
-      <p>Rating: {{ mostRecent.rating }}</p>
-      <p>Note: {{ mostRecent.note }}</p>
+      <transition name="fade" appear>
+        <table>
+          <tbody>
+            <tr>
+              <td>Bean:</td>
+              <td>
+                {{ mostRecent.bean.name }} ({{ mostRecent.bean.roast_profile }}
+                roast)
+              </td>
+            </tr>
+            <tr>
+              <td>Grind size:</td>
+              <td>{{ mostRecent.grind_size }}</td>
+            </tr>
+            <tr>
+              <td>Batch size:</td>
+              <td>{{ mostRecent.batch_size_oz }} oz.</td>
+            </tr>
+            <tr>
+              <td>Rating:</td>
+              <td>{{ mostRecent.rating }}</td>
+            </tr>
+            <tr>
+              <td>Note:</td>
+              <td>{{ mostRecent.note }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </transition>
     </div>
     <div v-else class="recent-batch-empty">no recent batches</div>
   </section>
@@ -23,11 +47,49 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 .recent-batch {
   @apply p-2;
   @apply mb-2;
   @apply bg-yellow-200;
   @apply rounded-md;
+}
+
+h2 {
+  @apply text-center;
+  text-shadow: 2px 2px 2px #bbf;
+}
+
+table {
+  width: 100%;
+}
+
+tr:nth-child(2n-1) {
+  @apply bg-orange-200;
+}
+
+td {
+  min-width: 150px;
+  @apply border;
+  @apply border-gray-400;
+}
+
+td:first-child {
+  @apply font-bold;
+}
+
+td:nth-child(2) {
+  @apply px-2;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: 300ms ease-out;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  transform: scaleY(0);
 }
 </style>
